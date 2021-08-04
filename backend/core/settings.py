b6f829918cdc40950e3979c6105c906ad7bc9b9a
grkm <garslan13@gmail.com>
2021-08-04 22:32:02 +0300

    Project level permission is set to IsAuthenticated

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -151,7 +151,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Django Rest Framework
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny'
+        'rest_framework.permissions.IsAuthenticated'
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
